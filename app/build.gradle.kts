00e302791bbfa85d5d01593487ebff8ad00f8c60
Hai Nguyen <anhhai282003@gmail.com>
2024-09-16 21:21:54 +0700

    change title, logo, version app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "vn.nhom2eaut.bookchefk12"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
